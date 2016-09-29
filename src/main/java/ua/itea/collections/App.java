47ef4019ad7cd2d3870518cac137acc47e9e1f61
e-polischuk <z.polischuk@gmail.com>
2016-09-30 00:43:35 +0300

    working version of HashMap with 'put', 'get' and 'remove' algorithms

--- src/main/java/ua/itea/collections/App.java
+++ src/main/java/ua/itea/collections/App.java
@@ -9,11 +9,26 @@ public class App
     public static void main( String[] args )
     {
 	MyHashMap<String, Integer> map = new MyHashMap<String, Integer>();
-        System.out.println( map.put("one", 1) );
-        System.out.println( map.put("two", 2) );
-        System.out.println( map.put("one", 3) );
-        System.out.println( map.put("one", 4) );
-        System.out.println( map.put("one", 5) );
+        map.put("one", 1);
+        map.put("two", 2);
+        map.put("three", 3);
+        map.put("four", 4);
+        map.put("five", 5);
+        map.remove("two");
+        map.put("six", 10);
+        map.put("7", 15);
+        map.put("sahur", 45);
+        map.remove("three");
+        map.remove("one");
+        map.remove("7");
+        map.remove("six");
+        map.put("one", 34);
+        map.put("four", Integer.MAX_VALUE >> 1);
+        map.put("sahur", 45);
+//        map.remove("two");
+        System.out.println(map.get("sahur"));
+        System.out.println(map.get("four"));
+        
         
     }
 }
--- src/main/java/ua/itea/collections/MyHashMap.java
+++ src/main/java/ua/itea/collections/MyHashMap.java
@@ -5,9 +5,9 @@ import java.util.*;
 @SuppressWarnings("unchecked")
 public class MyHashMap<K, V> extends AbstractMap<K, V> {
     private int size;
-    int capacity;
-    Entry<K, V>[] table;
-    double loadFactor;
+    private int capacity;
+    private Entry<K, V>[] table;
+    private double loadFactor;
 
     public MyHashMap() {
 	loadFactor = 0.75;
@@ -15,70 +15,123 @@ public class MyHashMap<K, V> extends AbstractMap<K, V> {
 	table = new Entry[capacity];
     }
 
-    public V put(K key, V value) {
-	int index = 0;
-	if (null != key) index = hash(key);
-	Entry<K, V> e = table[index];
-	while (e != null) {
-	    if (null == e.key && null == key || key.equals(e.key)) return null;
-	    e = e.next;
-	}
-	if (size > capacity * loadFactor) increase();
-	Entry<K, V> newEntry = new Entry<K, V>(hash(key), key, value, null);
-	return addEntry(newEntry);
+    public V get(Object key) {
+	return getEntry((K) key).getValue();
     }
 
-    public void increase() {
-	Entry<K, V>[] oldTable = table;
-	capacity <<= 1;
-	table = new Entry[capacity];
+    public V remove(Object key) {
+	int index = 0;
+	if (null != key)
+	    index = hash((K) key);
+	if (table[index] != null) {
+	    Entry<K, V> pre = null;
+	    for (Entry<K, V> e = table[index]; e != null; e = e.next) {
+		if (key == e.key || (null != key && key.equals(e.key))) {
+		    if(pre == null) {
+			table[index] = e.next;
+		    } else {
+			pre.next = e.next;
+		    }
+		    V value = e.getValue();
+		    size--;
+		    System.out.println("Removed {key=" + key + ", value=" + value + "}; size = " + size
+			    + "; capacity = " + capacity);
+		    System.out.println(toString() + "\n");
+		    return value;
+		}
+		pre = e;
+	    }
+	}
+	throw new NoSuchElementException();
+    }
 
-	Entry<K, V> e = null;
-	for (int i = 0; i < oldTable.length; i++) {
-	    if (oldTable[i] != null) {
-		e = oldTable[i];
-		while (e != null) {
+    private Entry<K, V> getEntry(K key) {
+	int index = 0;
+	if (null != key)
+	    index = hash(key);
+	for (Entry<K, V> e = table[index]; e != null; e = e.next) {
+	    if (key == e.key || (null != key && key.equals(e.key)))
+		return e;
+	}
+	throw new NoSuchElementException();
+    }
+
+    public V put(K key, V value) {
+	int index = 0;
+	if (null != key)
+	    index = hash(key);
+	for (Entry<K, V> e = table[index]; e != null; e = e.next) {
+	    if ((key == e.key || (null != key && key.equals(e.key)))
+		    && (value == e.value || (null != value && value.equals(e.value))))
+		return null;
+	}
+	if (size >= capacity * loadFactor)
+	    resize();
+	V val = addEntry(new Entry<K, V>(key, value));
+	if (val == null)
+	    size++;
+	System.out.println("Added {key=" + key + ", value=" + value + "}; returned value = " + val + "; size = " + size
+		+ "; capacity = " + capacity);
+	System.out.println(toString() + "\n");
+	return val;
+    }
+
+    public void resize() {
+	if (capacity < Integer.MAX_VALUE >> 1) {
+	    Entry<K, V>[] oldTable = table;
+	    capacity <<= 1;
+	    table = new Entry[capacity];
+	    for (int i = 0; i < oldTable.length; i++) {
+		for (Entry<K, V> e = oldTable[i]; e != null; e = e.next) {
 		    addEntry(e);
-		    e = e.next;
 		}
 	    }
 	}
     }
 
-    public V addEntry(Entry<K, V> e) {
+    private V addEntry(Entry<K, V> newEnt) {
 	int index = 0;
-	if (null != e.key)
-	    index = hash(e.key);
-	if (null == table[index]) {
-	    table[index] = e;
-	    return e.value;
+	if (null != newEnt.key)
+	    index = hash(newEnt.key);
+	if (table[index] == null) {
+	    table[index] = newEnt;
 	} else {
-	    Entry<K, V> entry = table[index];
-	    while (entry.next != null) {
-		if(entry.key == e.key) return entry.setValue(e.value);
-		entry = entry.next;
-	    }
-	    entry = e;
-	    return e.value;
+	    Entry<K, V> e = table[index];
+	    do {
+		if (newEnt.key == e.key || (newEnt.key != null && newEnt.key.equals(e.key)))
+		    return e.setValue(newEnt.value);
+		if (e.next != null)
+		    e = e.next;
+	    } while (e.next != null);
+	    e.next = newEnt;
 	}
+	return null;
     }
 
     public int hash(K key) {
+	if (null == key)
+	    return 0;
 	return key.hashCode() & (capacity - 1);
     }
 
-    public static class Entry<K, V> implements Map.Entry<K, V> {
-	int hash;
+    public String toString() {
+	StringBuffer sb = new StringBuffer("{");
+	Iterator<Entry<K, V>> it = new EntryIterator();
+	while (it.hasNext()) {
+	    sb.append("(" + it.next().toString() + ")");
+	}
+	sb.append("}");
+	return sb.toString();
+    }
+
+    private static class Entry<K, V> implements Map.Entry<K, V> {
 	K key;
 	V value;
 	Entry<K, V> next;
 
-	Entry(int hash, K key, V value, Entry<K, V> next) {
-	    this.hash = hash;
+	Entry(K key, V value) {
 	    this.key = key;
 	    this.value = value;
-	    this.next = next;
-
 	}
 
 	public final K getKey() {
@@ -95,36 +148,62 @@ public class MyHashMap<K, V> extends AbstractMap<K, V> {
 	    return oldValue;
 	}
 
-	@SuppressWarnings("rawtypes")
-	public boolean equals(Object o) {
-	    if (!(o instanceof Map.Entry))
-		return false;
-	    Map.Entry e = (Map.Entry) o;
-	    Object k1 = getKey();
-	    Object k2 = e.getKey();
-	    if (k1 == k2 || (k1 != null && k1.equals(k2))) {
-		Object v1 = getValue();
-		Object v2 = e.getValue();
-		if (v1 == v2 || (v1 != null && v1.equals(v2)))
-		    return true;
-	    }
-	    return false;
-	}
-
-	public int hashCode() {
-	    return (key == null ? 0 : key.hashCode()) ^ (value == null ? 0 : value.hashCode());
-	}
-
 	public String toString() {
 	    return getKey() + "=" + getValue();
 	}
 
-	void recordAccess(MyHashMap<K, V> m) {
+    }
+
+    private abstract class MyHashIterator<E> implements Iterator<E> {
+	Entry<K, V> current;
+	// int expectedModCount;
+	protected int passed;
+
+	MyHashIterator() {
+	    current = table[0];
+	    passed = 0;
 	}
 
-	void recordRemoval(MyHashMap<K, V> m) {
+	public boolean hasNext() {
+	    if (passed < size)
+		return true;
+	    return false;
 	}
 
+	public Entry<K, V> nextEntry() {
+	    if (current != null && current.next != null) {
+		passed++;
+		current = current.next;
+		return current;
+	    }
+	    for (int i = hash(current == null ? null : current.key) + 1; i < table.length; i++) {
+		if (table[i] != null) {
+		    passed++;
+		    current = (Entry<K, V>) table[i];
+		    return current;
+		}
+	    }
+	    return null;
+	}
+
+    }
+
+    public class KeyIterator extends MyHashIterator<K> {
+	public K next() {
+	    return nextEntry().getKey();
+	}
+    }
+
+    public class ValueIterator extends MyHashIterator<V> {
+	public V next() {
+	    return nextEntry().getValue();
+	}
+    }
+
+    public class EntryIterator extends MyHashIterator<Entry<K, V>> {
+	public Entry<K, V> next() {
+	    return nextEntry();
+	}
     }
 
     @Override
@@ -132,71 +211,5 @@ public class MyHashMap<K, V> extends AbstractMap<K, V> {
 	// TODO Auto-generated method stub
 	return null;
     }
-
-    // class MyIterator
-
-    /*
-     * Adds a new entry with the specified key, value and hash code to the
-     * specified bucket. It is the responsibility of this method to resize the
-     * table if appropriate.
-     *
-     * Subclass overrides this to alter the behavior of put method.
-     *
-     * void addEntry(int hash, K key, V value, int bucketIndex) { if ((size >=
-     * threshold) && (null != table[bucketIndex])) { resize(2 * table.length);
-     * hash = (null != key) ? hash(key) : 0; bucketIndex = indexFor(hash,
-     * table.length); }
-     * 
-     * createEntry(hash, key, value, bucketIndex); }
-     **
-     * 
-     * Like addEntry except that this version is used when creating entries as
-     * part of Map construction or "pseudo-construction" (cloning,
-     * deserialization). This version needn't worry about resizing the table.
-     *
-     * Subclass overrides this to alter the behavior of HashMap(Map), clone, and
-     * readObject.
-     *
-     * void createEntry(int hash, K key, V value, int bucketIndex) { Entry<K,V>
-     * e = table[bucketIndex]; table[bucketIndex] = new Entry<>(hash, key,
-     * value, e); size++; }
-     * 
-     * private abstract class HashIterator<E> implements Iterator<E> {
-     * Entry<K,V> next; // next entry to return int expectedModCount; // For
-     * fast-fail int index; // current slot Entry<K,V> current; // current entry
-     * 
-     * HashIterator() { expectedModCount = modCount; if (size > 0) { // advance
-     * to first entry Entry[] t = table; while (index < t.length && (next =
-     * t[index++]) == null) ; } }
-     * 
-     * public final boolean hasNext() { return next != null; }
-     * 
-     * final Entry<K,V> nextEntry() { if (modCount != expectedModCount) throw
-     * new ConcurrentModificationException(); Entry<K,V> e = next; if (e ==
-     * null) throw new NoSuchElementException();
-     * 
-     * if ((next = e.next) == null) { Entry[] t = table; while (index < t.length
-     * && (next = t[index++]) == null) ; } current = e; return e; }
-     * 
-     * public void remove() { if (current == null) throw new
-     * IllegalStateException(); if (modCount != expectedModCount) throw new
-     * ConcurrentModificationException(); Object k = current.key; current =
-     * null; HashMap.this.removeEntryForKey(k); expectedModCount = modCount; } }
-     * 
-     * private final class ValueIterator extends HashIterator<V> { public V
-     * next() { return nextEntry().value; } }
-     * 
-     * private final class KeyIterator extends HashIterator<K> { public K next()
-     * { return nextEntry().getKey(); } }
-     * 
-     * private final class EntryIterator extends HashIterator<Map.Entry<K,V>> {
-     * public Map.Entry<K,V> next() { return nextEntry(); } }
-     * 
-     * // Subclass overrides these to alter behavior of views' iterator() method
-     * Iterator<K> newKeyIterator() { return new KeyIterator(); } Iterator<V>
-     * newValueIterator() { return new ValueIterator(); }
-     * Iterator<Map.Entry<K,V>> newEntryIterator() { return new EntryIterator();
-     * }
-     */
 
 }
